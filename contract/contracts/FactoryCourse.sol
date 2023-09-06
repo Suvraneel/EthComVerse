e4916f6c37f78c1a6f409776aad3ef25773c28b6
Aayush Gupta <guptaaayush432@gmail.com>
2023-09-06 16:15:54 +0530

    [contract] updated FactoryCourse.sol

--- contract/contracts/FactoryCourse.sol
+++ contract/contracts/FactoryCourse.sol
@@ -1,21 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 import "./Course.sol";
 
 contract FactoryCourse {
-    /// events
-    event CreateNewCourse(
-        string uri,
-        uint supply,
-        uint nftPrice,
-        address factoryContractAddress,
-        address indexed courseAddress
-    );
-    event WithdrawMoney(address withdrawAddress, uint amount);
-
+    /// STATE VARIABLES ///
     /// @dev factory contract owner
     address private factoryOwner;
+
+    /// @notice  number of Courses created
+    uint256 private numOfCourse;
 
     /**
      * @notice struct to store all the data of Course
@@ -28,21 +22,27 @@ contract FactoryCourse {
         address factoryOwner;
     }
 
+    /// EVENTS  ///
+    event CreateNewCourse(
+        string uri,
+        uint supply,
+        uint nftPrice,
+        address factoryContractAddress,
+        address indexed courseAddress
+    );
+    event WithdrawMoney(address withdrawAddress, uint amount);
+
     /**
      * @notice Mapping to store all the details of course created by the creator
      * @dev creator address -> course details in factoryCourseStruct datatype
      */
     mapping(address => factoryCourseStruct[]) private creatorCourses;
 
-    
     /**
      * @notice Mapping to store the courses created by the Creator
      * @dev Creator Address -> course contract addresses[]
      */
     mapping(address => address[]) private searchByAddress;
-
-    /// @notice  number of Courses created
-    uint256 private numOfCourse;
 
     /**
      * @dev constructor to set the owner address of this contract factory
@@ -52,7 +52,7 @@ contract FactoryCourse {
     }
 
     /**
-     * @dev : function to create new course and course address on searchBy Address
+     * @notice : function to create new course and course address on searchBy Address
      * @param _uri : NFT URI
      * @param _supply : Total supply of NFTs
      * @param _nftPrice : Price of the NFT
@@ -63,7 +63,7 @@ contract FactoryCourse {
         uint256 _supply,
         uint _nftPrice,
         address _creatorAddress
-    ) public {
+    ) external {
         Course course = new Course(
             _uri,
             _supply,
@@ -76,15 +76,6 @@ contract FactoryCourse {
         unchecked {
             ++numOfCourse;
         }
-
-        // emit CreateNewCourse event
-        emit CreateNewCourse(
-            _uri,
-            _supply,
-            _nftPrice,
-            address(this),
-            _creatorAddress
-        );
 
         // Add the new Course to the mapping
         creatorCourses[_creatorAddress].push(
@@ -99,13 +90,21 @@ contract FactoryCourse {
 
         // search the profile by using creator address
         searchByAddress[_creatorAddress].push(address(course));
+
+        // emit CreateNewCourse event
+        emit CreateNewCourse(
+            _uri,
+            _supply,
+            _nftPrice,
+            address(this),
+            _creatorAddress
+        );
     }
 
-    // function to withdraw the fund from contract factory
     /**
-     * @dev : function to withdraw funds
+     * @notice function to withdraw funds
      * @param _amount : amount owner want to withdraw
-     * @param _withdrawAddress: address coursefactoryowner wants to withdraw to
+     * @param _withdrawAddress: address factoryOwner wants to withdraw to
      */
     function withdraw(
         uint256 _amount,
@@ -125,39 +124,54 @@ contract FactoryCourse {
         emit WithdrawMoney(_withdrawAddress, _amount);
     }
 
-    // Getter functins
-    // get the balance of the contract
+    ///                         GETTER FUNCTIONS
+
+    /**
+     * @notice function to get the balance of the contract
+     */
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
     // get the address of this contract
-    function getAddressOfFactoryContract() public view returns (address) {
+    /**
+     * @notice function to get the address of the Factory contract
+     */
+    function getFactoryContractAddress() public view returns (address) {
         return address(this);
     }
 
-    // get the address of FactoryCourse contract owner
-    function getAddressOffactoryOwner() public view returns (address) {
+    /**
+     * @notice function to get the address of the Factory owner
+     */
+    function getFactoryOwnerAddress() public view returns (address) {
         return factoryOwner;
     }
 
-    // get all the course NFT addresses deployed by this creator address
-    function getSearchByAddress(
+    /**
+     * @notice function to get the addresses of all course NFT deloyed by the creator
+     * @param _creatorAddress Address of the creator
+     */
+    function getAllNftAddresses(
         address _creatorAddress
     ) public view returns (address[] memory) {
         return searchByAddress[_creatorAddress];
     }
 
     /**
-     * @notice function to get the number of courses created 
+     * @notice function to get the number of courses created
      */
-    function getCourseCount() external view returns(uint){
+    function getCourseCount() external view returns (uint) {
         return numOfCourse;
     }
 
-    // receive function is used to receive Ether when msg.data is empty
+    /**
+     * @notice receive function is used to receive Native Token when msg.data is empty
+     */
     receive() external payable {}
 
-    // Fallback function is used to receive Ether when msg.data is NOT empty
+    /**
+     * @notice fallback function is used to receive Native Token when msg.data is NOT empty
+     */
     fallback() external payable {}
 }
