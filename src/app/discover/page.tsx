df749e425e03e41b968f7fa076caec2a6c82dac3
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-09-29 01:51:43 +0530

    Update page.tsx

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -7,7 +7,7 @@ const Discover: NextPage = async () => {
     console.log(productsData)
     return (
         <div className="w-full h-full p-10 lg:px-40 flex justify-evenly flex-wrap">
-            {
+            {/* {
                 productsData.map((item: any) => {
                     return(
                     <div key={item._id} className='w-[25vw] aspect-square flex flex-row justify-center items-center relative rounded-lg border border-cardGray-700 hover:border-gray-700 group overflow-clip'>
@@ -22,7 +22,7 @@ const Discover: NextPage = async () => {
                         />
                     </div>)
                 })
-            }
+            } */}
         </div>
     )
 }
